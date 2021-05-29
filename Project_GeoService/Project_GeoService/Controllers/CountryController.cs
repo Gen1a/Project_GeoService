6e75433b6c160562a65640491ecb3682ea0b3980
Frederik De Brouwer <frederik.debrouwer@cegeka.com>
2021-05-29 13:48:30 +0200

    Added rrequest handling for api/country and api/country/id

--- Project_GeoService/Project_GeoService/Controllers/CountryController.cs
+++ Project_GeoService/Project_GeoService/Controllers/CountryController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Project_GeoService.Exceptions;
 using Project_GeoService.Interfaces;
+using Project_GeoService.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +19,27 @@ namespace Project_GeoService.Controllers
         public CountryController(ICountryRepository repository)
         {
             this._repository = repository;
+        }
+
+        // GET: api/country
+        [HttpGet]
+        public IEnumerable<Country> Get()
+        {
+            return _repository.GetAll();
+        }
+
+        // GET: api/country/{id}
+        [HttpGet("{id}", Name ="Get")]
+        public ActionResult<Country> Get(int id)
+        {
+            try
+            {
+                return _repository.GetCountry(id);
+            }
+            catch (CountryException ex)
+            {
+                return NotFound(ex.Message);
+            }
         }
     }
 }
