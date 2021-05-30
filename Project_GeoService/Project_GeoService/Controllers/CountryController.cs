6271ae62e80cf722839a2dd4945ad193aba53e0d
Frederik De Brouwer <frederik.debrouwer@cegeka.com>
2021-05-30 14:18:47 +0200

    Added POST, PUT, PATCH and DELETE requests

--- Project_GeoService/Project_GeoService/Controllers/CountryController.cs
+++ Project_GeoService/Project_GeoService/Controllers/CountryController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Project_GeoService.Exceptions;
 using Project_GeoService.Interfaces;
@@ -42,14 +43,14 @@ namespace Project_GeoService.Controllers
                 }
                 else return _repository.GetAll().ToList();
             }
-            catch(CountryException ex)
+            catch (CountryException ex)
             {
                 return NotFound(ex.Message);
             }
         }
 
         // GET: api/country/{id}
-        [HttpGet("{id}", Name ="Get")]
+        [HttpGet("{id}", Name = "Get")]
         [HttpHead("{id}")]
         public ActionResult<Country> Get(long id)
         {
@@ -60,6 +61,73 @@ namespace Project_GeoService.Controllers
             catch (CountryException ex)
             {
                 return NotFound(ex.Message);
+            }
+        }
+
+        // POST
+        [HttpPost]
+        public ActionResult<Country> Post([FromBody] Country country)
+        {
+            _repository.AddCountry(country);
+            return CreatedAtAction(nameof(Get), new { Id = country.Id }, country);
+        }
+
+        // PUT: Tries to execute a FULL update of a resource
+        [HttpPut("{id}")]
+        public ActionResult Put(long id, [FromBody] Country country)
+        {
+            if (country == null || id != country.Id) return BadRequest();
+
+            try
+            {
+                _repository.UpdateCountry(country);
+                return new NoContentResult();
+            }
+            catch (CountryException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Adding new country to database...");
+                _repository.AddCountry(country);
+                return CreatedAtAction(nameof(Get), new { Id = country.Id }, country);
+            }
+        }
+
+        // PATCH: Tries to execute a PARTIAL update of a resource
+        [HttpPatch("{id}")]
+        public ActionResult<Country> Patch(long id, [FromBody] JsonPatchDocument<Country> countryPatchDoc)
+        {
+            if (countryPatchDoc == null) return BadRequest();
+
+            try
+            {
+                Country c = _repository.GetCountry(id);
+                countryPatchDoc.ApplyTo(c); // country = array of patches to execute (included in the request body)
+                return c;
+            }
+            catch (CountryException ex)
+            {
+                Console.WriteLine("Error during PATCH request: " + ex.Message);
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error during PATCH request: " + ex.Message);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE
+        [HttpDelete("{id}")]
+        public ActionResult Delete(long id)
+        {
+            try
+            {
+                _repository.RemoveCountry(_repository.GetCountry(id));
+                return NoContent();
+            }
+            catch(CountryException ex)
+            {
+                return NotFound("Error during DELETE request: " + ex.Message);
             }
         }
     }
