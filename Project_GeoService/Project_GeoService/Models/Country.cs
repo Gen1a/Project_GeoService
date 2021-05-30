94c12ceb8329eec0220e23a7fef49439134ebbf6
Frederik De Brouwer <frederik.debrouwer@cegeka.com>
2021-05-30 19:29:48 +0200

    Added Validation attributes

--- Project_GeoService/Project_GeoService/Models/Country.cs
+++ Project_GeoService/Project_GeoService/Models/Country.cs
@@ -1,4 +1,6 @@
-﻿namespace Project_GeoService.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_GeoService.Models
 {
     public class Country
     {
@@ -13,9 +15,16 @@
 
         public Country(long id, string name, string capital, string continent) : this(name, capital, continent) => Id = id;
 
+        [Range(1, long.MaxValue, ErrorMessage ="{0} must be greater than {1}.")]
         public long Id { get; set; }
+
+        [StringLength(50, MinimumLength = 2)]
         public string Name { get; set; }
+
+        [StringLength(50, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 2)]
         public string Capital { get; set; }
+
+        [StringLength(25, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 4)]
         public string Continent { get; set; }
     }
 }
