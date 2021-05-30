6ed230603b50ed8eb35b5724d009db2f8857f59b
Frederik De Brouwer <frederik.debrouwer@cegeka.com>
2021-05-30 14:18:29 +0200

    Upgrade Target framework to .NET 5.0 + Add nuget packages

--- Project_GeoService/Project_GeoService/Startup.cs
+++ Project_GeoService/Project_GeoService/Startup.cs
@@ -26,7 +26,9 @@ namespace Project_GeoService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers(setup => setup.ReturnHttpNotAcceptable = true).AddXmlDataContractSerializerFormatters();
+            services.AddControllers(setup => setup.ReturnHttpNotAcceptable = true)
+                .AddNewtonsoftJson()
+                .AddXmlDataContractSerializerFormatters();
             services.AddSingleton<ICountryRepository, CountryRepository>();
         }
 
