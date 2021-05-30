521c9c6d8f35fa810f1f051f7ee41b4717479f9e
Frederik De Brouwer <frederik.debrouwer@cegeka.com>
2021-05-30 14:59:21 +0200

    Added exception constructor with 0 parameters

--- Project_GeoService/Project_GeoService/Exceptions/CountryException.cs
+++ Project_GeoService/Project_GeoService/Exceptions/CountryException.cs
@@ -4,6 +4,7 @@ namespace Project_GeoService.Exceptions
 {
     public class CountryException : Exception
     {
+        public CountryException() { }
         public CountryException(string message) : base(message) { }
     }
 }
